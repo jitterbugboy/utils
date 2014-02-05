c404a04e1142093d3ac1842b233523712885be59
jitterbugboy <jitternielsen@gmail.com>
2014-02-05 21:09:11 +0100

    reformat code

--- tests/config/karma.conf.js
+++ tests/config/karma.conf.js
@@ -58,8 +58,8 @@ module.exports = function (config) {
 		},
 
 		coverageReporter : {
-			//type : 'text'
-			//     dir : 'tests/coverage/'
+			type : 'text',
+			dir  : 'tests/coverage/'
 		},
 
 
--- tests/specs/addEventsSpec.js
+++ tests/specs/addEventsSpec.js
@@ -1,79 +1,109 @@
-define(['events/addEvents', 'events/trigger', 'sinon', 'jasminejquery'], function (addEvents, trigger, sinon, jasminejquery) {
+define(
+	[
+		'events/addEvents',
+		'events/trigger',
+		'sinon',
+		'jasminejquery'
+	]
+	, function (addEvents, trigger, sinon, jasminejquery) {
 
-    describe('Events-addEvents testSuite', function () {
-        var div
-            , handler;
-
-
-
-        beforeEach(function () {
-            var f = jasminejquery.getFixtures();
-            f.fixturesPath = 'base/tests/fixtures/';
-            f.load('nestedDiv.html');
-            div =  document.getElementById('inner');
-            handler = sinon.spy();
-        });
-
-        afterEach(function () {
-            div = null;
-            handler.reset();
-        });
+		describe('Events-addEvents testSuite', function () {
+			var div
+				, handler;
 
 
-        it('can add click event to div sinon', function () {
-            var handler = sinon.spy();
-            addEvents(div, 'click', handler);
-            trigger(div, 'click');
-            expect(handler.calledOnce).toBe(true);
-        });
+			beforeEach(function () {
+				var f = jasminejquery.getFixtures();
+
+				f.fixturesPath = 'base/tests/fixtures/';
+
+				f.load('nestedDiv.html');
+
+				div = document.getElementById('inner');
+
+				handler = sinon.spy();
+			});
+
+			afterEach(function () {
+				div = null;
+
+				handler.reset();
+			});
 
 
-        it('has clicked dom element as this reference', function () {
-            addEvents(div, 'click', handler);
-            trigger(div, 'click');
-            expect(handler.calledOn(div)).toBe(true);
-        });
+			it('can add click event to div sinon', function () {
+				var handler = sinon.spy();
+
+				addEvents(div, 'click', handler);
+
+				trigger(div, 'click');
+
+				expect(handler.calledOnce).toBe(true);
+			});
 
 
-        it('can add have clicked event object passed as argument', function () {
-            var spyCall
-                , target;
+			it('has clicked dom element as this reference', function () {
+				addEvents(div, 'click', handler);
 
-            addEvents(div, 'click', handler);
-            trigger(div, 'click');
-            spyCall = handler.getCall(0);
-            //kinda ducktyping here - idk how to check for the actual eventobject in another way
-            target = spyCall.args[0].target;
-            expect(target).toBe(div);
-        });
+				trigger(div, 'click');
 
-        it('can bubble',function () {
-            var handlerOuter = sinon.spy()
-                ,outerDiv = $('#outer')[0];
-            addEvents(outerDiv,'click', handlerOuter, false);
-            addEvents(div,'click', handler);
-            trigger(div, 'click');
-            expect(handlerOuter.calledOnce).toBe(true);
-            expect(handler.calledOnce).toBe(true);
-
-            expect(handler.calledBefore(handlerOuter)).toBe(true);
-
-        });
-
-        it('can catch capture', function () {
-            var handlerOuter = sinon.spy()
-                ,outerDiv = $('#outer')[0];
-
-            addEvents(outerDiv,'click', handlerOuter, true);
-            addEvents(div,'click', handler);
-            trigger(div, 'click');
-
-            expect(handlerOuter.calledOnce).toBe(true);
-            expect(handler.calledOnce).toBe(true);
-            expect(handlerOuter.calledBefore(handler)).toBe(true);
-        });
+				expect(handler.calledOn(div)).toBe(true);
+			});
 
 
+			it('can add have clicked event object passed as argument', function () {
+				var spyCall
+					, target;
 
-    });
-});
+				addEvents(div, 'click', handler);
+
+				trigger(div, 'click');
+
+				spyCall = handler.getCall(0);
+
+				//kinda ducktyping here - idk how to check for the actual eventobject in another way
+				target = spyCall.args[0].target;
+
+				expect(target).toBe(div);
+			});
+
+			it('can bubble', function () {
+				var handlerOuter = sinon.spy()
+					, outerDiv = $('#outer')[0];
+
+				addEvents(outerDiv, 'click', handlerOuter, false);
+
+				addEvents(div, 'click', handler);
+
+				trigger(div, 'click');
+
+
+				expect(handlerOuter.calledOnce).toBe(true);
+
+				expect(handler.calledOnce).toBe(true);
+
+				expect(handler.calledBefore(handlerOuter)).toBe(true);
+
+			});
+
+			it('can catch capture', function () {
+				var handlerOuter = sinon.spy()
+					, outerDiv = $('#outer')[0];
+
+				addEvents(outerDiv, 'click', handlerOuter, true);
+
+				addEvents(div, 'click', handler);
+
+				trigger(div, 'click');
+
+
+				expect(handlerOuter.calledOnce).toBe(true);
+
+				expect(handler.calledOnce).toBe(true);
+
+				expect(handlerOuter.calledBefore(handler)).toBe(true);
+			});
+
+
+		});
+	});
